3ef9ffd65df94c9d5bf85541c729a23b7dccfde7
Sergey Nozhka <nojkasa@icloud.com>
2023-08-03 23:03:12 +0400

    Initial Commit

---
+++ KD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KD
+//
+//  Created by Сергей Ножка on 03.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
